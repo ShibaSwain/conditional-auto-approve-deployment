55ce7f81615d1183e5daaeb12ab92160089ddfec
Shiba Swain <shiba@srjfn.onmircosoft.com>
2024-12-22 12:24:51 +0000

    check context data

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
-import { getInput } from "@actions/core";
+import { getInput, setFailed } from "@actions/core";
+import { context, getOctokit } from "@actions/github";
 
 export async function run() {
-    console.log("Hello World" + getInput("name"));
+  const token = getInput("gh-token");
+  const label = getInput("label");
+
+//  const octokit = getOctokit(token);
+//  const pullRequest = context.payload.pull_request;
+
+  try {
+    console.log("context", context.payload);
+  } catch (error) {
+    setFailed((error as Error)?.message ?? "Unknown error");
+  }
 }
 
 if (!process.env.JEST_WORKER_ID) {
-    run();
-  }
-  
+  run();
+}
