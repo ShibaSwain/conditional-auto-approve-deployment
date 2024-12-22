1530a66f5db1030ad07feb4b8ac86870e6f9fe73
Shiba Swain <shiba@srjfn.onmircosoft.com>
2024-12-22 12:13:34 +0000

    issue fixed - no index file found inside dist folder

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello World");
+import { getInput } from "@actions/core";
+
+export async function run() {
+    console.log("Hello World" + getInput("name"));
+}
+
+if (!process.env.JEST_WORKER_ID) {
+    run();
+  }
+  
